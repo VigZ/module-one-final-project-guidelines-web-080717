797eded6f3a90c7117c6e59de35035392ffc7214
github username <github email address>
2017-08-22 12:51:40 -0400

    made methods into class methods and made dynamic

--- lib/api_class.rb
+++ lib/api_class.rb
@@ -2,20 +2,20 @@ require "pry"
 require "rest-client"
 require "JSON"
 class ApiController
-	def get_api_data
+	def self.get_api_data(cuisine_id)
 		data_array = []
 		i = 0
 		while i < 100
-			data_array << get_and_truncate(i)
+			data_array << get_and_truncate(i, cuisine_id)
 			i += 20
 		end
 		data_array.flatten
 
 	end
 
-	def get_and_truncate(arg)
+	def self.get_and_truncate(start,cuisine_id)
 		wanted_keys = ["name","url","address","location","price_range","user_rating"]
-		restaurant_array = RestClient.get("https://developers.zomato.com/api/v2.1/search?entity_id=280&entity_type=city&start=#{arg}&count=20&cuisines=25&apikey=6a33a0b0f8ca2cec230e87d13ef64ef3")
+		restaurant_array = RestClient.get("https://developers.zomato.com/api/v2.1/search?entity_id=280&entity_type=city&start=#{start}&count=20&cuisines=#{cuisine_id}&apikey=6a33a0b0f8ca2cec230e87d13ef64ef3")
 		parsed = JSON.parse(restaurant_array)["restaurants"]
 		parsed.each do |restaurant_hash|
 			restaurant_hash["restaurant"].delete_if{|key, value| !wanted_keys.include?(key)}
@@ -23,8 +23,3 @@ class ApiController
 	end
 
 end
-
-lol = ApiController.new
-array = lol.get_api_data
-binding.pry
-"hi"
