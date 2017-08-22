3244def93e8929c96bf76cd9be03f9e63e28a9ee
github username <github email address>
2017-08-22 12:42:50 -0400

    loop fixed

--- lib/api_class.rb
+++ lib/api_class.rb
@@ -3,19 +3,28 @@ require "rest-client"
 require "JSON"
 class ApiController
 	def get_api_data
-		# data_array = []
-		# data_array << RestClient.get(https://developers.zomato.com/api/v2.1/search?entity_id=280&entity_type=city&start=0&count=20&cuisines="#{cuisine_var}")
-		# data_array << RestClient.get(https://developers.zomato.com/api/v2.1/search?entity_id=280&entity_type=city&start=21&count=20&cuisines="#{cuisine_var}")
-		# data_array << RestClient.get(https://developers.zomato.com/api/v2.1/search?entity_id=280&entity_type=city&start=41&count=20&cuisines="#{cuisine_var}")
-		# data_array << RestClient.get(https://developers.zomato.com/api/v2.1/search?entity_id=280&entity_type=city&start=61&count=20&cuisines="#{cuisine_var}")
-		# data_array << RestClient.get(https://developers.zomato.com/api/v2.1/search?entity_id=280&entity_type=city&start=81&count=20&cuisines="#{cuisine_var}")
-	end
-	wanted_keys = ["name","url","address","location","price_range","user_rating"]
-	restaurant_array = RestClient.get("https://developers.zomato.com/api/v2.1/search?entity_id=280&entity_type=city&start=0&count=20&cuisines=25&apikey=6a33a0b0f8ca2cec230e87d13ef64ef3")
-	parsed = JSON.parse(restaurant_array)["restaurants"]
-	parsed.each do |restaurant_hash|
-		restaurant_hash["restaurant"].delete_if{|key, value| !wanted_keys.include?(key)}
+		data_array = []
+		i = 0
+		while i < 100
+			data_array << get_and_truncate(i)
+			i += 20
 		end
-		binding.pry
+		data_array.flatten
+
+	end
+
+	def get_and_truncate(arg)
+		wanted_keys = ["name","url","address","location","price_range","user_rating"]
+		restaurant_array = RestClient.get("https://developers.zomato.com/api/v2.1/search?entity_id=280&entity_type=city&start=#{arg}&count=20&cuisines=25&apikey=6a33a0b0f8ca2cec230e87d13ef64ef3")
+		parsed = JSON.parse(restaurant_array)["restaurants"]
+		parsed.each do |restaurant_hash|
+			restaurant_hash["restaurant"].delete_if{|key, value| !wanted_keys.include?(key)}
+			end
+	end
 
 end
+
+lol = ApiController.new
+array = lol.get_api_data
+binding.pry
+"hi"
