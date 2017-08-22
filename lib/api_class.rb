1d7e5f096a6eb44e6e16dca8e4c7dfb9be410e7f
github username <github email address>
2017-08-22 11:16:43 -0400

    chugging along

--- lib/api_class.rb
+++ lib/api_class.rb
@@ -10,11 +10,12 @@ class ApiController
 		# data_array << RestClient.get(https://developers.zomato.com/api/v2.1/search?entity_id=280&entity_type=city&start=61&count=20&cuisines="#{cuisine_var}")
 		# data_array << RestClient.get(https://developers.zomato.com/api/v2.1/search?entity_id=280&entity_type=city&start=81&count=20&cuisines="#{cuisine_var}")
 	end
-	wanted_keys = ["name","url","address","locality","price_range","user_rating"]
+	wanted_keys = ["name","url","address","location","price_range","user_rating"]
 	restaurant_array = RestClient.get("https://developers.zomato.com/api/v2.1/search?entity_id=280&entity_type=city&start=0&count=20&cuisines=25&apikey=6a33a0b0f8ca2cec230e87d13ef64ef3")
-	test = JSON.parse(restaurant_array)["restaurants"]
-	test.each do |restaurant_hash|
-		restaurant_hash.keys.delete_if{|key| !wanted_keys.include?(key) }
+	parsed = JSON.parse(restaurant_array)["restaurants"]
+	parsed.each do |restaurant_hash|
+		restaurant_hash["restaurant"].keys.delete_if{|key| !wanted_keys.include?(key)}
 		binding.pry
 		end
+
 end
