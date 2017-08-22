df31b697398447bf1b1917d52351d919f270875b
github username <github email address>
2017-08-22 11:22:16 -0400

    have the array of truncated hashes

--- lib/api_class.rb
+++ lib/api_class.rb
@@ -14,8 +14,8 @@ class ApiController
 	restaurant_array = RestClient.get("https://developers.zomato.com/api/v2.1/search?entity_id=280&entity_type=city&start=0&count=20&cuisines=25&apikey=6a33a0b0f8ca2cec230e87d13ef64ef3")
 	parsed = JSON.parse(restaurant_array)["restaurants"]
 	parsed.each do |restaurant_hash|
-		restaurant_hash["restaurant"].keys.delete_if{|key| !wanted_keys.include?(key)}
-		binding.pry
+		restaurant_hash["restaurant"].delete_if{|key, value| !wanted_keys.include?(key)}
 		end
+		binding.pry
 
 end
