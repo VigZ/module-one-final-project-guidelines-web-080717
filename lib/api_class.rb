532bbc09ffc0ea87a24e74593e55b517d67c4136
sophiehelf <sophie.helf@gmail.com>
2017-08-22 10:09:12 -0400

    add to api_class

--- lib/api_class.rb
+++ lib/api_class.rb
@@ -7,4 +7,13 @@ class ApiController
 		data_array << RestClient.get(https://developers.zomato.com/api/v2.1/search?entity_id=280&entity_type=city&start=61&count=20&cuisines="#{cuisine_var}")
 		data_array << RestClient.get(https://developers.zomato.com/api/v2.1/search?entity_id=280&entity_type=city&start=81&count=20&cuisines="#{cuisine_var}")
 	end
+	wanted_keys = ["name","url","address","locality","price_range","user_rating"]
+	restaurant_array = RestClient.get(https://developers.zomato.com/api/v2.1/search?entity_id=280&entity_type=city&start=0&count=20&cuisines="#{cuisine_var}")["restaurants"]
+	restaurant_array.each do |restaurant_hash|
+		restaurant_hash.keys.delete_if{|key| !wanted_keys.include?(key) }
+		end
+	end
+		
+	end
+
 end
