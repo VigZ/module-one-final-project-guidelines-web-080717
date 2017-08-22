14df2fac684c739b3b3a779d1df5d61f3dbf0518
sophiehelf <sophie.helf@gmail.com>
2017-08-22 09:42:50 -0400

    api_class, selections, updated cli

--- lib/api_class.rb
+++ lib/api_class.rb
@@ -1,5 +1,10 @@
 class ApiController
-	def get_api_data
-		RestClient.get(https://developers.zomato.com/api/v2.1/search?entity_id=280&entity_type=city&start=0&count=20&cuisines="#{cuisine_var}")
+	def get_api_data 
+		data_array = []
+		data_array << RestClient.get(https://developers.zomato.com/api/v2.1/search?entity_id=280&entity_type=city&start=0&count=20&cuisines="#{cuisine_var}")
+		data_array << RestClient.get(https://developers.zomato.com/api/v2.1/search?entity_id=280&entity_type=city&start=21&count=20&cuisines="#{cuisine_var}")
+		data_array << RestClient.get(https://developers.zomato.com/api/v2.1/search?entity_id=280&entity_type=city&start=41&count=20&cuisines="#{cuisine_var}")
+		data_array << RestClient.get(https://developers.zomato.com/api/v2.1/search?entity_id=280&entity_type=city&start=61&count=20&cuisines="#{cuisine_var}")
+		data_array << RestClient.get(https://developers.zomato.com/api/v2.1/search?entity_id=280&entity_type=city&start=81&count=20&cuisines="#{cuisine_var}")
 	end
 end
---
+++ lib/selections.rb
@@ -0,0 +1,30 @@
+def hash_selection
+
+	cuisine_hash = {
+		"american" => 1,
+		"chinese" => 25,
+		"barbecue" => 193,
+		"breakfast" => 182,
+		"cafe" => 30,
+		"cajun" => 491,
+		"deli" => 192,
+		"desserts" => 100,
+		"fast food" => 40,
+		"french" => 45,
+		"greek" => 156,
+		"indian" => 148,
+		"italian" => 55,
+		"japanese" => 60,
+		"korean" => 67,
+		"mexican" =>73,
+		"pizza" => 82,
+		"puerto rican" => 361,
+		"sushi" => 177,
+		"steak" => 141,
+		"thai" => 95,
+		"vietnamese" =>99
+	}
+
+	
+
+end
